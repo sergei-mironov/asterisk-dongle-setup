ff2c115cbdca62b6ea6fb9300620009ce8c365fa
Sergey Mironov <grrwlf@gmail.com>
2020-08-02 12:13:41 +0300

    Add debug configs

--- default.nix
+++ default.nix
@@ -17,11 +17,13 @@ let
       asterisk-chan-dongle = stdenv.mkDerivation {
         name = "asterisk-chan-dongle";
 
-        src = pkgs.fetchgit {
-          url = "https://github.com/wdoekes/asterisk-chan-dongle";
-          rev = "0d1bad55b55940cecc9b196c72e17fc254a3d5a7";
-          sha256 = "sha256:1nvbc5azqgpc7vwyc0mskqxpnrz8a65a37r6n7nisw3r9q7axasy";
-        };
+        # src = pkgs.fetchgit {
+        #   url = "https://github.com/wdoekes/asterisk-chan-dongle";
+        #   rev = "0d1bad55b55940cecc9b196c72e17fc254a3d5a7";
+        #   sha256 = "sha256:1nvbc5azqgpc7vwyc0mskqxpnrz8a65a37r6n7nisw3r9q7axasy";
+        # };
+
+        src = ../asterisk-chan-dongle;
 
         preConfigure = ''
           ./bootstrap
@@ -75,9 +77,31 @@ let
           astrundir => /tmp/asterisk/
           astlogdir => /tmp/asterisk/
           astsbindir => ${asterisk}/sbin
+
+          [options]
+          verbose = 255
+          debug = 255
+          runuser = root		; The user to run as.
+          rungroup = root		; The group to run as.
           EOF
 
-          cp -v ${asterisk-chan-dongle.src}/etc/dongle.conf $out/etc/asterisk
+          # cp -v ${asterisk-chan-dongle.src}/etc/dongle.conf $out/etc/asterisk
+          cat >$out/etc/asterisk/dongle.conf <<EOF
+          [general]
+          interval=15
+          smsdb=/tmp/asterisk/smsdb
+          csmsttl=600
+
+          [dongle0]
+          data=/dev/ttyUSB0		; tty port for AT commands; 		no default value
+          audio=/dev/ttyUSB1		; tty port for audio connection; 	no default value
+
+          ; or you can omit both audio and data together and use imei=123456789012345 and/or imsi=123456789012345
+          ;  imei and imsi must contain exactly 15 digits !
+          ;  imei/imsi discovery is available on Linux only
+          imei=123456789012345
+          imsi=123456789012345
+          EOF
         '';
       };
 
