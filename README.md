f0add876de8d152f4e59a62aef59649601483ffa
Sergey Mironov <grrwlf@gmail.com>
2020-10-11 17:46:22 +0300

    Update

--- README.md
+++ README.md
@@ -64,3 +64,7 @@ References
 * GSMCTL https://www.unix.com/man-page/debian/8/gsmctl/
   - Homepage looks inactive
 * MMCLI https://www.freedesktop.org/software/ModemManager/man/1.0.0/mmcli.8.html
+* Asterisk+Dongle setup guide (in Russian)
+  http://linux.mixed-spb.ru/asterisk/dongle_app1.php
+
+
--- default.nix
+++ default.nix
@@ -28,7 +28,7 @@ let
         };
       };
 
-      pyst = python.buildPythonPackage rec {
+      pyst2 = python.buildPythonPackage rec {
         pname = "pyst2";
         version = "0.5.1";
         propagatedBuildInputs = with python ; [ six ];
@@ -38,6 +38,15 @@ let
           sha256 = "sha256:1kw13g7wldzrnnr9vcm97m4c8pv801hl4fl7q88jvz0q9caz9s07";
         };
       };
+
+      mypyps = ppkgs: with ppkgs; [
+        pandas
+        requests
+        pyst2
+        ipython
+      ];
+
+      mypython = pkgs.python37.withPackages mypyps;
 
       asterisk-chan-dongle = stdenv.mkDerivation {
         name = "asterisk-chan-dongle";
@@ -117,6 +126,42 @@ let
           smsdb=/tmp/asterisk/smsdb
           csmsttl=600
 
+          [defaults]
+          context=dongle-incoming			; context for incoming calls
+          group=0				; calling group
+          rxgain=0			; increase the incoming volume; may be negative
+          txgain=0			; increase the outgoint volume; may be negative
+          autodeletesms=yes		; auto delete incoming sms
+          resetdongle=yes			; reset dongle during initialization with ATZ command
+          u2diag=-1			; set ^U2DIAG parameter on device (0 = disable everything except modem function) ; -1 not use ^U2DIAG command
+          usecallingpres=yes		; use the caller ID presentation or not
+          callingpres=allowed_passed_screen ; set caller ID presentation		by default use default network settings
+          disablesms=no			; disable of SMS reading from device when received
+                            ;  chan_dongle has currently a bug with SMS reception. When a SMS gets in during a
+                            ;  call chan_dongle might crash. Enable this option to disable sms reception.
+                            ;  default = no
+
+          language=en			; set channel default language
+          mindtmfgap=45			; minimal interval from end of previews DTMF from begining of next in ms
+          mindtmfduration=80		; minimal DTMF tone duration in ms
+          mindtmfinterval=200		; minimal interval between ends of DTMF of same digits in ms
+
+          callwaiting=auto		; if 'yes' allow incoming calls waiting; by default use network settings
+                              ; if 'no' waiting calls just ignored
+          disable=no			; OBSOLETED by initstate: if 'yes' no load this device and just ignore this section
+
+          initstate=start			; specified initial state of device, must be one of 'stop' 'start' 'remote'
+                              ;   'remove' same as 'disable=yes'
+
+          exten=+1234567890		; exten for start incoming calls, only in case of Subscriber Number not available!, also set to CALLERID(ndid)
+
+          dtmf=relax			; control of incoming DTMF detection, possible values:
+                          ;   off	   - off DTMF tones detection, voice data passed to asterisk unaltered
+                          ;              use this value for gateways or if not use DTMF for AVR or inside dialplan
+                          ;   inband - do DTMF tones detection
+                          ;   relax  - like inband but with relaxdtmf option
+                          ;  default is 'relax' by compatibility reason
+
           [dongle0]
           data=/dev/ttyUSB0		; tty port for AT commands; 		no default value
           audio=/dev/ttyUSB1		; tty port for audio connection; 	no default value
@@ -124,8 +169,17 @@ let
           ; or you can omit both audio and data together and use imei=123456789012345 and/or imsi=123456789012345
           ;  imei and imsi must contain exactly 15 digits !
           ;  imei/imsi discovery is available on Linux only
-          imei=123456789012345
-          imsi=123456789012345
+          ; imei=123456789012345
+          ; imsi=123456789012345
+          EOF
+
+          rm $out/etc/asterisk/extensions.conf
+          cat >$out/etc/asterisk/extensions.conf <<"EOF"
+          [general]
+
+          [dongle-incoming]
+          exten => sms,1,Verbose(Incoming SMS)
+          exten => sms,n,Hangup()
           EOF
         '';
       };
